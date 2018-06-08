bc201bbe0ed29b249ec6eefd3f24ec5f42858177
Marcus Grant <marcusfg@gmail.com>
2018-06-08 00:01:00 -0400

    fix boilerplate structure

--- src/app.jsx
+++ src/app.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const App = () => (
-  <div className="app">
-    <h1>React Boilerplate</h1>
+  <div className="app__container">
+    <h1>TimeLord</h1>
     <p>Make sure to delete .git and README.md before starting fresh</p>
   </div>
 );
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import App from './app';
+import App from './app';
 
 import './main.scss';
 
 /* eslint-disable */
-const App = () => (
-  <div>
-    <h1>React Boilerplate</h1>
-    <p>Make sure to delete .git and README.md before starting fresh</p>
-  </div>
-);
+
+// const App = () => (
+//   <div>
+//     <h1>React Boilerplate</h1>
+//     <p>Make sure to delete .git and README.md before starting fresh</p>
+//   </div>
+// );
 
 render(<App />, document.getElementById('react-root'));
 /* eslint-enable */
